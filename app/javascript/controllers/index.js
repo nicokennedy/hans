b5039cb729e54488a39a9bb25ad8b590c6fcd349
nicolas kennedy <nico@mvpbuilders.tech>
2026-05-09 19:24:29 -0300

    Fix datepicker loading

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import DatepickerController from "./datepicker_controller"
-application.register("datepicker", DatepickerController)
